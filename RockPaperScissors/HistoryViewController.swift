35b2b62c2156999036d3d7c7864a0eea7e7cc3cc
yuanjun221 <yuanjun221@gmail.com>
2016-06-08 14:44:44 +0800

    add cell background color

--- RockPaperScissors/HistoryViewController.swift
+++ RockPaperScissors/HistoryViewController.swift
@@ -20,21 +20,29 @@ class HistoryViewController: UIViewController, UITableViewDataSource {
         let cell = tableView.dequeueReusableCellWithIdentifier("history") as! HistoryTableCell
         let match = history[indexPath.row]
         
-        cell.matchResultLabel.text = matchResult(match)
+        let (resultString, color) = matchResult(match)
+        (cell.matchResultLabel.text, cell.backgroundColor) = (resultString, color)
         cell.playerImage.image = imageForRPS(match.p1)
         cell.computerImage.image = imageForRPS(match.p2)
         
         return cell
     }
     
-    func matchResult(match: RPSMatch) -> String {
+    func matchResult(match: RPSMatch) -> (String, UIColor) {
+        var resultString: String!
+        var color: UIColor!
+        
         if match.p1 == match.p2 {
-            return "Tie."
-        } else if match.p1.defeats(match.p2){
-            return "Win!"
+            resultString = "Tie."
+            color = UIColor.whiteColor()
+        } else if match.p1.defeats(match.p2) {
+            resultString = "Win!"
+            color = UIColor(red: 1.0, green: 0.75, blue: 0.8, alpha: 1.0)
         } else {
-            return "Loss."
+            resultString = "Loss."
+            color = UIColor(red: 0.9, green: 1, blue: 0.94, alpha: 1.0)
         }
+        return (resultString, color)
     }
     
     func imageForRPS(rps:RPS) -> UIImage {
